fdf4b89cd8855e0c2f7f23a6b4486de488ee11e7
natsuto-kun <natsuto391@gmail.com>
2023-03-28 00:14:23 +0900

    記事一覧とブログページの作成

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,19 +1,19 @@
-import { getCollection } from "astro:content"
+import { getCollection } from "astro:content";
 
 export interface Blog {
-    title: string;
-    tags: string[];
-    date: string;
-    slug: string;
-    body: string;
+  title: string;
+  tags: string[];
+  date: string;
+  slug: string;
+  body: string;
 }
 
 export async function getAllBlogs(): Promise<Blog[]> {
-    const blogs = await getCollection("blog");
-    return blogs
-      .map((blog) => ({ ...blog.data, slug: blog.slug, body: blog.body}))
-        .sort(
-            (a, z) =>
-            new Date(z.date).getMilliseconds() - new Date(a.date).getMilliseconds()
-        );
+  const blogs = await getCollection("blog"); // 設定ファイルで指定したキーを設定
+  return blogs
+    .map((blog) => ({ ...blog.data, slug: blog.slug, body: blog.body }))
+    .sort(
+      (a, z) =>
+        new Date(z.date).getMilliseconds() - new Date(a.date).getMilliseconds()
+    );
 }
